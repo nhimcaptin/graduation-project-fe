1961656a33693be855c15f2f00ae3e316cc1e3cb
Hoang Thi Ngoc Bich <hoangbich181203@gmail.com>
2023-09-18 21:55:20 +0700

    [BE] Base project

---
+++ BE_NodeJS/src/route/web.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+
+    router.get('/', (req, res) =>{
+        return res.send('hi ae')
+    });
+    
+    return app.use("/", router);
+
+}
+
+module.exports = initWebRoutes;
