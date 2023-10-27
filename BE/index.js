17a6f5b27cda72e487691c1c03d9fe2901daff94
Hoang Thi Ngoc Bich <hoangbich181203@gmail.com>
2023-10-27 15:07:00 +0700

    [BE] test upload IMAGE

--- BE/index.js
+++ BE/index.js
@@ -13,6 +13,12 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import session  from "express-session";
 
+import {upload, uploadMultiple} from "./middlewares/multer.js";
+import {getStorage, ref ,uploadBytesResumable} from 'firebase/storage';
+import {signInWithEmailAndPassword, createUserWithEmailAndPassword} from "firebase/auth";
+import {auth} from "./config/firebase.config.js";
+
+
 const app = express();
 dotenv.config();
 
@@ -30,6 +36,60 @@ const connect = async () => {
 mongoose.connection.on("disconnected", () => {
   console.log("mongoDB disconnected!");
 });
+
+//UPLOAD IMAGE
+async function uploadImage(file, quantity) {
+  const storageFB = getStorage();
+
+  await signInWithEmailAndPassword(auth, process.env.FIREBASE_USER, process.env.FIREBASE_AUTH)
+
+  if (quantity === 'single') {
+      const dateTime = Date.now();
+      const fileName = `images/${dateTime}`
+      const storageRef = ref(storageFB, fileName)
+      const metadata = {
+          contentType: file.type,
+      }
+      await uploadBytesResumable(storageRef, file.buffer, metadata);
+      return fileName
+  }
+
+  if (quantity === 'multiple') {
+      for(let i=0; i < file.images.length; i++) {
+          const dateTime = Date.now();
+          const fileName = `images/${dateTime}`
+          const storageRef = ref(storageFB, fileName)
+          const metadata = {
+              contentType: file.images[i].mimetype,
+          }
+
+          const saveImage = await Image.create({imageUrl: fileName});
+          file.item.imageId.push({_id: saveImage._id});
+          await file.item.save();
+
+          await uploadBytesResumable(storageRef, file.images[i].buffer, metadata);
+
+      }
+      return
+  }
+
+}
+
+app.post('/test-upload', upload, async (req, res) => {
+  const file = {
+      type: req.file.mimetype,
+      buffer: req.file.buffer
+  }
+  try {
+      const buildImage = await uploadImage(file, 'single'); 
+      res.send({
+          status: "SUCCESS",
+          imageName: buildImage
+      })
+  } catch(err) {
+      console.log(err);
+  }
+})
 
 //middlewares
 app.use(cors())
