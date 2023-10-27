a6ff6a8f67d80b95c7b18446293fbcaf4453b57a
Van Tinh <nguyentinh02082002@gmail.com>
2023-10-27 15:28:16 +0700

    [UPDATE] [BE] updateSubservice and DeleteSubService

--- BE/routes/subServices.js
+++ BE/routes/subServices.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { creatSubservice, updateSubservice } from "../controllers/subService.js";
+import { creatSubservice, deleteSubservice, getSubservice, updateSubservice } from "../controllers/subService.js";
 
 const router = express.Router();
 
 router.post("/create/:id", creatSubservice);
 router.put("/updateSubService/:id", updateSubservice);
+router.delete("/deleteSubservice/:id", deleteSubservice);
+router.get("/getallSubService", getSubservice);
 
 
 export default router
