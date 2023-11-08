c431684f45a6f006bed194f30c91d9a50e552e0c
Van Tinh <nguyentinh02082002@gmail.com>
2023-11-09 02:32:07 +0700

    [UPDATE] [BE] deleteNews

--- BE/routes/news.js
+++ BE/routes/news.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { createNews, getNews, updateNews } from "../controllers/news.js";
+import { createNews, deleteNews, getNews, updateNews } from "../controllers/news.js";
 
 const router = express.Router();
 
 router.post("/createNews", createNews);
 router.put("/updateNews/:id", updateNews);
 router.get("/getNews", getNews);
-// router.delete("/deleteDentalKnowledge/:id", deleteDentalKnowledge);
+router.delete("/deleteNews/:id", deleteNews);
 
 export default router
