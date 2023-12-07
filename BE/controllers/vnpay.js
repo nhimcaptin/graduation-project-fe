ec0135477aa9014a22f084a2e44b441703daeb25
Trần Danh Doanh <doanh.tran@bmbsoft.com.vn>
2023-12-07 13:05:13 +0700

    [UPDATE] update refund vnpay

--- BE/controllers/vnpay.js
+++ BE/controllers/vnpay.js
@@ -4,6 +4,7 @@ import querystring from "qs";
 import moment from "moment";
 import request from "request";
 import Booking from "../models/Booking.js";
+import { MESSAGE_ERROR } from "../const/messages.js";
 
 export const createdUrl = (req, res, next) => {
   process.env.TZ = "Asia/Ho_Chi_Minh";
@@ -124,7 +125,11 @@ export const querydr = (req, res, next) => {
     async function (error, response, body) {
       try {
         if (body?.vnp_ResponseCode == "00") {
-          await Booking.findOneAndUpdate({ _id: vnp_TxnRef }, { $set: { statusPaymentOrder: "Done" } }, { new: true });
+          await Booking.findOneAndUpdate(
+            { _id: vnp_TxnRef },
+            { $set: { statusPaymentOrder: "Done", transactionDate: vnp_TransactionDate } },
+            { new: true }
+          );
         }
         return res.status(200).json(body);
       } catch (error) {
@@ -132,6 +137,124 @@ export const querydr = (req, res, next) => {
       }
     }
   );
+};
+
+export const refund = async (req, res, next) => {
+  try {
+    const { userId, id } = req.body;
+    const item = await Booking.findById(id);
+    if (!item) {
+      return res.status(400).json(MESSAGE_ERROR.CANNOT_FIND);
+    }
+    if (item?.statusPaymentOrder != "Done") {
+      await Booking.findOneAndUpdate(
+        { _id: id },
+        { $set: { status: "Cancel", statusPaymentOrder: "Cancel" } },
+        { new: true }
+      );
+      return res.status(200).json("Success");
+    }
+    process.env.TZ = "Asia/Ho_Chi_Minh";
+    let date = new Date();
+
+    let vnp_TmnCode = config.get("vnp_TmnCode");
+    let secretKey = config.get("vnp_HashSecret");
+    let vnp_Api = config.get("vnp_Api");
+
+    let vnp_TxnRef = id;
+    // let vnp_TxnRef = "05152613";
+    let vnp_TransactionDate = item.transactionDate;
+    // let vnp_TransactionDate = "20231205152636";
+    let vnp_Amount = Number(item.totalAmount) * 100;
+    let vnp_TransactionType = "02";
+    let vnp_CreateBy = userId;
+
+    let vnp_RequestId = moment(date).format("HHmmss");
+    let vnp_Version = "2.1.0";
+    let vnp_Command = "refund";
+    let vnp_OrderInfo = "Hoan tien GD ma:" + vnp_TxnRef;
+
+    let vnp_IpAddr =
+      req.headers["x-forwarded-for"] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress;
+
+    let vnp_CreateDate = moment(date).format("YYYYMMDDHHmmss");
+
+    let vnp_TransactionNo = "0";
+
+    let data =
+      vnp_RequestId +
+      "|" +
+      vnp_Version +
+      "|" +
+      vnp_Command +
+      "|" +
+      vnp_TmnCode +
+      "|" +
+      vnp_TransactionType +
+      "|" +
+      vnp_TxnRef +
+      "|" +
+      vnp_Amount +
+      "|" +
+      vnp_TransactionNo +
+      "|" +
+      vnp_TransactionDate +
+      "|" +
+      vnp_CreateBy +
+      "|" +
+      vnp_CreateDate +
+      "|" +
+      vnp_IpAddr +
+      "|" +
+      vnp_OrderInfo;
+    let hmac = crypto.createHmac("sha512", secretKey);
+    let vnp_SecureHash = hmac.update(new Buffer(data, "utf-8")).digest("hex");
+
+    let dataObj = {
+      vnp_RequestId: vnp_RequestId,
+      vnp_Version: vnp_Version,
+      vnp_Command: vnp_Command,
+      vnp_TmnCode: vnp_TmnCode,
+      vnp_TransactionType: vnp_TransactionType,
+      vnp_TxnRef: vnp_TxnRef,
+      vnp_Amount: vnp_Amount,
+      vnp_TransactionNo: vnp_TransactionNo,
+      vnp_CreateBy: vnp_CreateBy,
+      vnp_OrderInfo: vnp_OrderInfo,
+      vnp_TransactionDate: vnp_TransactionDate,
+      vnp_CreateDate: vnp_CreateDate,
+      vnp_IpAddr: vnp_IpAddr,
+      vnp_SecureHash: vnp_SecureHash,
+    };
+
+    request(
+      {
+        url: vnp_Api,
+        method: "POST",
+        json: true,
+        body: dataObj,
+      },
+      function (error, response, body) {
+        if (body?.vnp_ResponseCode == "00") {
+          (async () => {
+            await Booking.findOneAndUpdate(
+              { _id: id },
+              { $set: { status: "Cancel", statusPaymentOrder: "Cancel" } },
+              { new: true }
+            );
+          })();
+          return res.status(200).json(body);
+        } else {
+          return res.status(500).json(MESSAGE_ERROR.STATUS_500);
+        }
+      }
+    );
+  } catch (error) {
+    next(error);
+  }
 };
 
 function sortObject(obj) {
--- BE/routes/vnpay.js
+++ BE/routes/vnpay.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { verifyAdmin } from "../middlewares/verifyToken.js";
-import { createdUrl, querydr } from "../controllers/vnpay.js";
+import { createdUrl, querydr, refund } from "../controllers/vnpay.js";
 
 const router = express.Router();
 
 //CREATE
 router.post("/create_payment_url", createdUrl);
 router.post("/querydr", querydr);
+router.post("/refund", refund);
 
 export default router;
