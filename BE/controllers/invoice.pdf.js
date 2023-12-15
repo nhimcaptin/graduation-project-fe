e0add3e61530ba4b4c9e646ef888759c922d3e13
Nguyen Van Tinh <nguyentinh02082002@gmail.com>
2023-12-16 03:00:56 +0700

    [UPDATE] update invoice

--- BE/controllers/invoice.pdf.js
+++ BE/controllers/invoice.pdf.js
@@ -1,26 +1,56 @@
-import pdf from 'html-pdf';
+import pdf from "html-pdf";
+import config from "config";
 import invoicePdf from "../documents/invoicePdf.js";
-import {fileURLToPath}  from 'url' ;
-import {dirname}  from 'path' ;
-// const { dirname } = require('path');
+import { fileURLToPath } from "url";
+import { getStorage, ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+import { dirname } from "path";
+import request from "request";
+import * as fs from "fs";
 
+export const createPdf = (req, res, next) => {
+  const nameFile = `controllers/Invoice/PHIEU_KHAM_${new Date().getTime()}.pdf`;
+  let feat_pdf = config.get("feat_pdf");
+  pdf.create(invoicePdf(req.body), {}).toFile(nameFile, (err) => {
+    if (err) {
+      res.send(Promise.reject());
+    }
 
-export const createPdf = (req, res) => {
-    pdf.create(invoicePdf(req.body), {}).toFile('result.pdf', (err) => {
-        if(err) {
-            res.send(Promise.reject());
+    res.send(Promise.resolve());
+    const dataObj = {
+      nameFile,
+    };
+    request(
+      {
+        url: feat_pdf,
+        method: "POST",
+        json: true,
+        body: dataObj,
+      },
+      async function (error, response, body) {
+        try {
+          return res.status(200).json(body);
+        } catch (error) {
+          next(error);
         }
-
-        res.send(Promise.resolve());
-    });
+      }
+    );
+  });
 };
 
-// export const fetchPdf = (req, res) => {
-//     res.sendFile(`${__dirname}/result.pdf`)
-// };
-
-export const fetchPdf = (req, res) => {
+export const fetchPdf = async (req, res, next) => {
+  try {
+    const { nameFile } = req.body;
     const currentFile = fileURLToPath(import.meta.url);
-    const currentDir = dirname(currentFile);
-    res.sendFile(`${currentDir}/result.pdf`);
+    const storageFB = getStorage();
+    const storageRef = ref(storageFB, nameFile);
+    const fileBuffer = fs.readFileSync(nameFile);
+    const metadata = {
+      contentType: "application/pdf",
+    };
+    const snapshot = await uploadBytesResumable(storageRef, fileBuffer, metadata);
+    const downloadURL = await getDownloadURL(snapshot.ref);
+    res.status(200).send(downloadURL);
+  } catch (error) {
+    next(error);
+  }
 };
