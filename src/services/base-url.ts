6b74da0d62c6a2419a403f6267aa1f3816556b18
Hoang Thi Ngoc Bich <hoangbich181203@gmail.com>
2023-12-16 00:55:17 +0700

    ..

--- src/services/base-url.ts
+++ src/services/base-url.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8081/";
+export const BASE_URL = "http://localhost:8080/";
 // export const BASE_URL = 'https://taydo-api.onrender.com/';
 export const URL_LOCAL = "http://127.0.0.1:5173";
 export const IMG_URL =
