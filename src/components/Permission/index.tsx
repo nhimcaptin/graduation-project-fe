3193daaacc93c961e935ea873d8f0ab07fd387d5
Nguyen Van Tinh <nguyentinh02082002@gmail.com>
2023-12-22 00:38:11 +0700

    [UPDATE] router role

--- src/components/Permission/index.tsx
+++ src/components/Permission/index.tsx
@@ -3,10 +3,24 @@ import { Box } from "@mui/material";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { ACTION_ROLE_CODE } from "../../consts/permission";
+import ROUTERS_PATHS from "../../consts/router-paths";
 
 const Permission = (props: any) => {
   const { children, role } = props;
   const { permissions } = useSelector((state: any) => state.permission);
+  if (
+    role &&
+    role == "Dashboard" &&
+    !(permissions || []).some((permission: any) => {
+      let hasPermission = false;
+      if (typeof role === "string") hasPermission = permission?.resource?.code === role;
+      else if (typeof role === "object" && role.length && role.length > 0)
+        hasPermission = role.includes(permission?.resource?.code);
+      return hasPermission && (permission?.actions || []).some((action: any) => action.code === ACTION_ROLE_CODE.View);
+    })
+  ) {
+    return <Navigate to={ROUTERS_PATHS.BOOKING} replace />;
+  }
   if (
     role &&
     !(permissions || []).some((permission: any) => {
